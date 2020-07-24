8be71b41f3f5ee419d9bc42835f2157a199c2264
Hans <741581879@qq.com>
2020-07-24 15:55:04 +0800

    [Hans] test: Implete return car when fetch car given ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,7 +1,19 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private Map<CarTicket, Car> carTicketCarMap = new HashMap<>();
+
     public CarTicket parkCar(Car car) {
-        return new CarTicket();
+        CarTicket ticket = new CarTicket();
+        carTicketCarMap.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetchCar(CarTicket ticket) {
+        Car fetchedCar = carTicketCarMap.get(ticket);
+        return fetchedCar;
     }
 }
