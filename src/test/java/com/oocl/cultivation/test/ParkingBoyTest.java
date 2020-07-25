bbfe76f15589327794f512362a0dba17c048a218
Hans <741581879@qq.com>
2020-07-25 11:05:48 +0800

    [Story2][Hans] test: Add should_return_not_enough_position_when_park_space_is_full_given_car test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation.test;
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,8 @@ public class ParkingBoyTest {
         Car car = new Car();
 
         //when
-        String result = parkingBoy.receiveTicketFromCustomer(ticket);
+        parkingBoy.receiveTicketFromCustomer(ticket);
+        String result = parkingBoy.responseMessage();
 
         //then
         Assertions.assertEquals("Unrecognized parking ticket.", result);
@@ -27,9 +29,25 @@ public class ParkingBoyTest {
         ParkingBoy parkingBoy = new ParkingBoy();
 
         //when
-        String result = parkingBoy.receiveTicketFromCustomer(null);
+        parkingBoy.receiveTicketFromCustomer(null);
+        String result = parkingBoy.responseMessage();
 
-        //then
+                //then
         Assertions.assertEquals("Please provide your parking ticket.", result);
+    }
+
+    @Test
+    void should_return_not_enough_position_when_park_space_is_full_given_car() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy();
+
+        // when
+        for (int i = 0; i <= 18; i++) {
+            parkingBoy.parkCar(new Car());
+        }
+        String result = parkingBoy.responseMessage();
+        // then
+        Assertions.assertEquals("Not enough position.", result);
     }
 }
