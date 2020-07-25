d7b74640761455183cac5af3ba33aa7647d65c7d
Hans <741581879@qq.com>
2020-07-25 14:18:34 +0800

    [Story2][Hans] test: Modify should_return_not_enough_position_when_park_space_is_full_given_car test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -7,13 +7,15 @@ import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ParkingBoyTest {
     @Test
     void should_return_unrecognized_parking_ticket_when_fetch_car_given_wrong_ticket() {
         //given
         ParkingBoy parkingBoy = new ParkingBoy();
         CarTicket ticket = new CarTicket(false);
-        Car car = new Car();
 
         //when
         parkingBoy.receiveTicketFromCustomer(ticket);
@@ -39,15 +41,49 @@ public class ParkingBoyTest {
     @Test
     void should_return_not_enough_position_when_park_space_is_full_given_car() {
         // given
-        ParkingLot parkingLot = new ParkingLot();
-        ParkingBoy parkingBoy = new ParkingBoy();
+        ParkingLot parkingLot = new ParkingLot("0001");
+        ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(parkingLot));
 
         // when
-        for (int i = 0; i <= 18; i++) {
+        for (int i = 0; i < 15; i++) {
             parkingBoy.parkCar(new Car());
         }
         String result = parkingBoy.responseMessage();
+
         // then
         Assertions.assertEquals("Not enough position.", result);
     }
+
+    @Test
+    void should_return_0002_when_parking_1_is_full_given_car() {
+        // given
+        ParkingLot parkingLot1 = new ParkingLot("0001");
+        ParkingLot parkingLot2 = new ParkingLot("0002");
+        ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
+
+        // when
+        CarTicket ticket = null;
+        for (int i = 0; i < 11; i++) {
+            ticket = parkingBoy.parkCar(new Car());
+        }
+
+        // then
+        Assertions.assertEquals("0002", ticket.getParkingLotId());
+    }
+
+//    @Test
+//    void should_return_0001_when_parking1_is_more_than_parking2_given_car() {
+//        // given
+//        ParkingLot parkingLot1 = new ParkingLot("0001");
+//        ParkingLot parkingLot2 = new ParkingLot("0002");
+//        ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
+//
+//        // when
+//        CarTicket ticket = null;
+//
+//
+//        // then
+//        Assertions.assertEquals("0002", ticket.getParkingLotId());
+//    }
+
 }
