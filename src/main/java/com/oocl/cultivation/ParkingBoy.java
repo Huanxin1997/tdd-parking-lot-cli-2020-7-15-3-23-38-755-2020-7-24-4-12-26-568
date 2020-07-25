fb6bbe0506cf26e92b831656d768858220fbc753
Hans <741581879@qq.com>
2020-07-25 10:22:20 +0800

    [Story2][Hans] test: Implete return unrecognized parking ticket when fetch car given wrong ticket

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,14 @@
+package com.oocl.cultivation;
+
+import sun.security.krb5.internal.Ticket;
+
+public class ParkingBoy {
+
+    public Car fetchCar(Ticket ticket) {
+        return null;
+    }
+
+    public String receiveTicketFromCustomer(CarTicket ticket) {
+        return "Unrecognized parking ticket.";
+    }
+}
