44284c519bad4cd8ec871bff88bdb9c280180eaa
Hans <741581879@qq.com>
2020-07-26 17:04:12 +0800

    [Story6][Hans] test: Add should_return_parking_boy_2_when_specify_boy_given_parking_boy_2

--- src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ParkingLotServiceManagerTest {
@@ -14,12 +15,29 @@ public class ParkingLotServiceManagerTest {
     void should_return_true_when_add_parking_boy_capacity_given_1_parking_boy() {
         // given
         ParkingBoy parkingBoy = new ParkingBoy();
-        List parkingBoys = new ArrayList(Arrays.asList(parkingBoy));
+        List<ParkingBoy> parkingBoys = new ArrayList<>(Collections.singletonList(parkingBoy));
         ParkingLotServiceManager manager = new ParkingLotServiceManager(parkingBoys);
 
         // when
         Boolean result = manager.addParkingBoy(parkingBoy);
         // then
         Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void should_return_parking_boy_2_when_specify_boy_given_parking_boy_2() {
+        // given
+        ParkingBoy parkingBoy1 = new ParkingBoy();
+        ParkingBoy parkingBoy2 = new ParkingBoy();
+        ParkingBoy parkingBoy3 = new ParkingBoy();
+        List<ParkingBoy> parkingBoys = new ArrayList<>(Arrays.asList(parkingBoy1, parkingBoy2, parkingBoy3));
+        ParkingLotServiceManager manager = new ParkingLotServiceManager(parkingBoys);
+
+        // when
+        ParkingBoy specifiedParkingBoy = manager.specifyBoy(parkingBoy2);
+
+        // then
+        Assertions.assertNotNull(specifiedParkingBoy);
+        Assertions.assertEquals(parkingBoy2, specifiedParkingBoy);
     }
 }
